73297a368ccf58b60af07669fe86c29794a1837f
AlexKov3579 <kovalovs.alex1@gmail.com>
2022-05-09 20:13:26 +0300

    Initial commit

---
+++ login/models.py
@@ -0,0 +1,28 @@
+import uuid
+from django.db import models
+
+
+
+
+class Location (models.Model) :
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+class Company (models.Model) :
+    name = models.CharField(max_length=200)
+    contactNumber = models.CharField(max_length=15)
+    locationId = models.ForeignKey(Location, on_delete=models.CASCADE, blank= False, null= False)
+    
+class User (models.Model) :
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40)
+    email = models.EmailField
+    companyId = models.ForeignKey(Company, on_delete=models.CASCADE)
+    locationId = models.ForeignKey(Location, on_delete=models.CASCADE, blank= True, null= True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+class Password (models.Model) :
+    userId = models.OneToOneField(User,primary_key=True, on_delete=models.CASCADE)
+    password = models.CharField(max_length=64)
+    changedAt = models.DateTimeField(auto_now=True)
+# Create your models here.
---
+++ login/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def login(request):
+
+    if request.method == 'post':
+        pass
+    else:
+        pass
+    return render (request, "login.html")
+# Create your views here.
